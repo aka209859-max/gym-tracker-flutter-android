316b01070ff57747e6052fa46bedaa6f2958e07f
GYM MATCH AI Dev <ai-dev@gymmatch.jp>
2025-11-05 05:24:28 +0000

    クリーンアップ: 未使用のfl_chartインポート削除

--- lib/screens/workout/statistics_dashboard_screen.dart
+++ lib/screens/workout/statistics_dashboard_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:fl_chart/fl_chart.dart';
 import 'package:intl/intl.dart';
 
 /// Task 11: トレーニング統計ダッシュボード
